ae603c7a30d8f61697a92b02bfc40958f176ce15
Asier Ramos <uthopiko@gmail.com>
2017-02-23 23:21:00 +0100

    Commit 19

--- Image.php
+++ Image.php
@@ -23,6 +23,7 @@ class Image
     private $newVarForRebase12;
     private $newVarForRebase12;
     private $newVarForRebase12;
+    private $newVarForRebase12;
 
 
     function __construct($image)
