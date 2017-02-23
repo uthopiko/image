b085e71889ea88e47e12d35902b9a2bb1acacfeb
Asier Ramos <uthopiko@gmail.com>
2017-02-23 23:20:37 +0100

    Commit 18

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $value = $img->resizeImage(120,null);
 if($value){
 	echo "<br>Imagen ".$value." generada satisfactoriamente<br>";
 	echo "<br>Imagen2 ".$value." generada2 satisfactoriamente2<br>";
+	echo "<br>Imagen2 ".$value." generada2 satisfactoriamente2<br>";
 }else{
 	echo "<br>Imagen no generada, revisar errores<br>";
 	echo "<br>Imagen2 no generada2, revisar errores<br>";
